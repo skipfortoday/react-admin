7c79251fb88cc9a8e7abc848da8b85cd11ed620d
Rizqi Pratama <rev4rmy@gmail.com>
2021-01-15 10:05:53 +0700

    Detail Group Container

---
+++ src/containers/DetailGroupContainer.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Container } from "reactstrap";
+import BackGroup from "../components/BackGroup";
+import { connect } from "react-redux";
+import { getGroupDetail } from "../actions/groupAction";
+import DetailGroupComponent from "../components/DetailGroupComponent";
+
+class DetailGroupContainer extends Component {
+  componentDidMount() {
+    this.props.dispatch(getGroupDetail(this.props.match.params.GroupID));
+  }
+
+  render() {
+    return (
+      <Container>
+        <BackGroup />
+        <h1>Detail Group</h1>
+        <DetailGroupComponent />
+      </Container>
+    );
+  }
+}
+
+export default connect()(DetailGroupContainer);
