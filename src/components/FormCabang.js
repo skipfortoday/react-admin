94a7a0185d252223682bf369e26ed88bb0d67604
Rizqi Pratama <rev4rmy@gmail.com>
2021-01-19 14:44:07 +0700

    Add Form Cabang Info Home Cabang Info Menu Cabang

---
+++ src/components/FormCabang.js
@@ -0,0 +1,134 @@
+import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
+import { connect } from "react-redux";
+import { FormGroup, Col, Label, Input, Row, Button } from "reactstrap";
+import CabangValidation from "../validations/CabangValidation";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSave } from "@fortawesome/free-solid-svg-icons";
+
+const renderField = ({
+  input,
+  type,
+  placeholder,
+  label,
+  disabled,
+  readOnly,
+  meta: { touched, error, warning },
+}) => (
+  <Row>
+    <Col md="12">
+      <Label htmlFor="{input}" className="col-form-label">
+        {label}
+      </Label>
+    </Col>
+    <Col md="12">
+      <Input
+        {...input}
+        type={type}
+        placeholder={placeholder}
+        disabled={disabled}
+        readOnly={readOnly}
+      ></Input>
+      {touched &&
+        ((error && <p style={{ color: "red" }}>{error}</p>) ||
+          (warning && <p style={{ color: "brown" }}>{warning}</p>))}
+    </Col>
+  </Row>
+);
+
+const mapStateToProps = (state) => {
+  return {
+    initialValues: {
+      KodeCabang: state.Cabang.getCabangDetail.KodeCabang,
+      NamaCabang: state.Cabang.getCabangDetail.NamaCabang,
+      Alamat: state.Cabang.getCabangDetail.Alamat,
+      GeneralManagerID: state.Cabang.getCabangDetail.Pass,
+      hrdID: state.Cabang.getCabangDetail.hrdID,
+    },
+  };
+};
+
+class CabangComponent extends Component {
+  render() {
+    return (
+      <form onSubmit={this.props.handleSubmit}>
+        <FormGroup row>
+          <Col md={6}>
+            <FormGroup>
+              <Field
+                type="text"
+                name="KodeCabang"
+                component={renderField}
+                label="Kode Cabang :"
+              />
+            </FormGroup>
+          </Col>
+
+          <Col md={1}>
+            <FormGroup>
+              <Field
+                type="text"
+                name="NamaCabang"
+                component={renderField}
+                label="Nama Cabang :"
+              />
+            </FormGroup>
+          </Col>
+
+          <Col md={2}>
+            <FormGroup>
+              <Field
+                type="text"
+                name="Alamat"
+                component={renderField}
+                label="Alamat Cabang :"
+              />
+            </FormGroup>
+          </Col>
+
+          <Col md={2}>
+            <FormGroup>
+              <Field
+                type="text"
+                name="GeneralManagerID"
+                component={renderField}
+                label="General Manager :"
+              />
+            </FormGroup>
+          </Col>
+
+          <Col md={6}>
+            <FormGroup>
+              <Field
+                type="text"
+                name="hrdID"
+                component={renderField}
+                label="HRD Cabang:"
+              />
+            </FormGroup>
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Col md="12">
+            <FormGroup>
+              <Button
+                color="primary"
+                type="submit"
+                disabled={this.props.submitting}
+              >
+                <FontAwesomeIcon icon={faSave} /> SIMPAN
+              </Button>
+            </FormGroup>
+          </Col>
+        </FormGroup>
+      </form>
+    );
+  }
+}
+
+CabangComponent = reduxForm({
+  form: "formCreateCabang",
+  validate: CabangValidation,
+  enableReinitialize: true,
+})(CabangComponent);
+export default connect(mapStateToProps, null)(CabangComponent);
