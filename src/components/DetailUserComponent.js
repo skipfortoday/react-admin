863dacba9bc4bfd8f21fdb4ff6d9414feba7816d
Rizqi Pratama <rev4rmy@gmail.com>
2021-01-23 10:07:32 +0700

    Menampilkan Detail User Lengkap

--- src/components/DetailUserComponent.js
+++ src/components/DetailUserComponent.js
@@ -29,24 +29,64 @@ const DetailUserComponent = (props) => {
           <td>{props.getUserDetail.Nama}</td>
         </tr> 
         <tr>
+          <td width="200">Tanggal Lahir</td>
+          <td width="10">:</td>
+          <td>{props.getUserDetail.LahirHari}/{props.getUserDetail.LahirBulan}/{props.getUserDetail.LahirTahun}</td>
+        </tr>
+        <tr>
           <td width="200">Alamat</td>
           <td width="10">:</td>
           <td>{props.getUserDetail.Alamat}</td>
-        </tr>    
+        </tr>  
         <tr>
-          <td width="200">Tanggal Lahir</td>
+          <td width="200">HP</td>
           <td width="10">:</td>
-          <td>{props.getUserDetail.Hari}</td>
+          <td>{props.getUserDetail.HP}</td>
         </tr>
         <tr>
-          <td width="200">ID Groups</td>
+          <td width="200">Jabatan</td>
           <td width="10">:</td>
-          <td>{props.getUserDetail.GroupID}</td>
+          <td>{props.getUserDetail.Jabatan}</td>
         </tr>
         <tr>
-          <td width="200">Kode Cabang</td>
+          <td width="200">NamaCabang</td>
           <td width="10">:</td>
-          <td>{props.getUserDetail.KodeCabang}</td>
+          <td>{props.getUserDetail.NamaCabang}</td>
+        </tr>        
+        <tr>
+          <td width="200">Status</td>
+          <td width="10">:</td>
+          <td>{props.getUserDetail.Status}</td>
+        </tr>
+        <tr>
+          <td width="200">Tanggal Masuk Kerja</td>
+          <td width="10">:</td>
+          <td>{props.getUserDetail.MasukHari}/{props.getUserDetail.MasukBulan}/{props.getUserDetail.MasukTahun}</td>
+        </tr>
+        <tr>
+          <td width="200">Tanggal Awal Kontrak</td>
+          <td width="10">:</td>
+          <td>{props.getUserDetail.KontrakHari}/{props.getUserDetail.KontrakBulan}/{props.getUserDetail.KontrakTahun}</td>
+        </tr>
+        <tr>
+          <td width="200">Tanggal Mulai Cuti</td>
+          <td width="10">:</td>
+          <td>{props.getUserDetail.CutiHari}/{props.getUserDetail.CutiBulan}/{props.getUserDetail.CutiTahun}</td>
+        </tr>
+        <tr>
+          <td width="200">Tampilkan Lembur</td>
+          <td width="10">:</td>
+          <td>{props.getUserDetail.TampilkanTerlambat}</td>
+        </tr>
+        <tr>
+          <td width="200">Tampilkan Terlambat</td>
+          <td width="10">:</td>
+          <td>{props.getUserDetail.TampilkanTerlambat}</td>
+        </tr>
+        <tr>
+          <td width="200">Tanggal Keluar</td>
+          <td width="10">:</td>
+          <td>{props.getUserDetail.KeluarHari}/{props.getUserDetail.KeluarBulan}/{props.getUserDetail.KeluarTahun}</td>
         </tr>
       </tbody>
     </Table>
