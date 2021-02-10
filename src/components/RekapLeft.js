a7e440c0edb5c50df4723e3291c05b366a09bdf8
Rizqi Pratama <rev4rmy@gmail.com>
2021-02-10 12:01:08 +0700

    REKAP DONEE

--- src/components/RekapLeft.js
+++ src/components/RekapLeft.js
@@ -12,40 +12,127 @@ const mapStateToProps = (state) => {
   
 const RekapLeft = (props) => {
   return (
-    <Table size="sm">
+    <Table borderless size="sm">
       <tbody>
-        <tr>
-          <td>Ijin Terlambat</td>
-          <td>:</td>
-          <td>{props.getLaporanRekap.JumTerlambat}</td>
+        <tr style={{ lineHeight : '9px' }} >
+          <td style={{fontSize: '12px'}}>Ijin Terlambat</td>
+          <td style={{fontSize: '12px'}}>:</td>
+          <td style={{color: 'red', fontSize: '12px'}}>{props.getLaporanRekap.JumTerlambat}</td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}>Jumlah Sakit Bulan ini</td>
+          <td style={{fontSize: '12px'}}>:</td>
+          <td style={{fontSize: '12px'}}>{props.getLaporanRekap.JumTerlambat}</td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}>Jumlah Lembur</td>
+          <td style={{fontSize: '12px'}}>:</td>
+          <td style={{fontSize: '12px'}}>{props.getLaporanRekap.JumTerlambat}</td>
         </tr> 
-        <tr>
-          <td >Jumlah Terlambat</td>
-          <td >:</td>
-          <td>{props.getLaporanRekap.JumTerlambat}</td>
-          <td >=</td>
-          <td >{props.getLaporanRekap.RpPotonganTerlambat}</td>
+        <tr style={{ lineHeight : '9px' }}>
+          <td style={{fontSize: '12px'}}>Jumlah Terlambat</td>
+          <td style={{fontSize: '12px'}}>:</td>
+          <td style={{color: 'red',fontSize: '12px'}}>{props.getLaporanRekap.JumTerlambat}</td>
+          <td style={{fontSize: '12px'}}>=</td>
+          <td style={{color: 'red',fontSize: '12px'}}>{props.getLaporanRekap.RpPotonganTerlambat}</td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}>Jumlah Cuti Bulan ini</td>
+          <td style={{fontSize: '12px'}}>:</td>
+          <td style={{fontSize: '12px'}}>{props.getLaporanRekap.JumTerlambat}</td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}>Total Lembur</td>
+          <td style={{fontSize: '12px'}}>:</td>
+          <td style={{color: '#017580',fontSize: '12px'}}>{props.getLaporanRekap.JumTerlambat}</td>
         </tr>
-        <tr>
-          <td >Ijin Tidak Masuk</td>
-          <td >:</td>
-          <td>{props.getLaporanRekap.JumTerlambat}</td>
-          <td >=</td>
-          <td >{props.getLaporanRekap.RpPotonganTerlambat}</td>
+        <tr style={{ lineHeight : '9px' }}>
+          <td style={{fontSize: '12px'}}>Ijin Tidak Masuk</td>
+          <td style={{fontSize: '12px'}}>:</td>
+          <td style={{color: 'red',fontSize: '12px'}}>{props.getLaporanRekap.JumTerlambat}</td>
+          <td style={{fontSize: '12px'}}>=</td>
+          <td style={{color: 'red',fontSize: '12px'}}>{props.getLaporanRekap.RpPotonganTerlambat}</td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}>Ijin Tidak Masuk</td>
+          <td style={{fontSize: '12px'}}>:</td>
+          <td style={{fontSize: '12px'}}>{props.getLaporanRekap.JumTerlambat}</td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{color: 'red',fontSize: '12px'}}></td>
         </tr>
-        <tr>
-          <td >Trlmbt Kmbl Istrht</td>
-          <td >:</td>
-          <td>{props.getLaporanRekap.JumTerlambat}</td>
-          <td >=</td>
-          <td >{props.getLaporanRekap.RpPotonganTerlambat}</td>
+        <tr style={{ lineHeight : '9px' }}>
+          <td style={{fontSize: '12px'}}>Trlmbt Kmbl Istrht</td>
+          <td style={{fontSize: '12px'}}>:</td>
+          <td style={{color: 'red',fontSize: '12px'}}>{props.getLaporanRekap.JumTerlambat}</td>
+          <td style={{fontSize: '12px'}}>=</td>
+          <td style={{color: 'red',fontSize: '12px'}}>{props.getLaporanRekap.RpPotonganTerlambat}</td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}>--+</td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{color: 'red',fontSize: '12px'}}></td>
         </tr>
-        <tr>
-          <td >Total Potongan</td>
-          <td ></td>
-          <td ></td>
-          <td >=</td>
-          <td>{props.getLaporanRekap.JumTerlambat}</td>
+        <tr style={{ lineHeight : '9px' }}>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}>--------+</td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}>Total Tidak Masuk</td>
+          <td style={{fontSize: '12px'}}>:</td>
+          <td style={{fontSize: '12px'}}>{props.getLaporanRekap.JumTerlambat}</td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{color: 'red',fontSize: '12px'}}></td>
+        </tr>
+        <tr style={{ lineHeight : '9px' }}>
+          <td style={{fontSize: '12px'}}>Total Potongan</td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}>=</td>
+          <td style={{color: 'red',fontSize: '12px'}}>{props.getLaporanRekap.JumTerlambat}</td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}>Total Cuti Thn ini</td>
+          <td style={{fontSize: '12px'}}>:</td>
+          <td style={{fontSize: '12px'}}>{props.getLaporanRekap.JumTerlambat}</td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}>OFF</td>
+          <td style={{fontSize: '12px'}}>:</td>
+          <td style={{fontSize: '12px'}}>{props.getLaporanRekap.JumTerlambat}</td>
+        </tr>
+        <tr style={{ lineHeight : '9px' }}>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}>Sisa Cuti</td>
+          <td style={{fontSize: '12px'}}>:</td>
+          <td style={{fontSize: '12px'}}>{props.getLaporanRekap.JumTerlambat}</td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}>Jumlah Masuk Kantor</td>
+          <td style={{fontSize: '12px'}}>:</td>
+          <td style={{fontSize: '12px'}}>{props.getLaporanRekap.JumTerlambat}</td>
+        </tr>
+        <tr style={{ lineHeight : '9px' }}>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}>Cuti Khusus Bulan Ini</td>
+          <td style={{fontSize: '12px'}}>:</td>
+          <td style={{color: 'red',fontSize: '12px'}}>{props.getLaporanRekap.JumTerlambat}</td>
+          <td style={{fontSize: '12px'}}></td>
+          <td style={{fontSize: '12px'}}>Jumlah Dinas Luar</td>
+          <td style={{fontSize: '12px'}}>:</td>
+          <td style={{fontSize: '12px'}}>{props.getLaporanRekap.JumTerlambat}</td>
         </tr>
       </tbody>
     </Table>
