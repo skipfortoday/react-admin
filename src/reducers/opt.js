1d56d0da895a629d9846f45bbaeb255a694e483e
Rizqi Pratama <rev4rmy@gmail.com>
2021-02-17 14:37:38 +0700

    Create Action And Reduce OPT Group

--- src/reducers/opt.js
+++ src/reducers/opt.js
@@ -1,24 +1,30 @@
-import {
-    GET_OPT_USER,
-  } from "../actions/optAction"
-  
-  let initialState = {
-    getOptUser: false,
-    errorOptUser: false,
-  };
-  
-  const Opt = (state = initialState, action) => {
-    switch (action.type) {
-      case GET_OPT_USER:
-        return {
-          ...state,
-          getOptUser: action.payload.data,
-          errorOptUser: action.payload.errorMessage,
-        };
-      default:
-        return state;
-    }
-  };
-  
-  export default Opt ;
-  
+import { GET_OPT_USER , GET_OPT_GROUP } from "../actions/optAction";
+
+let initialState = {
+  getOptUser: false,
+  errorOptUser: false,
+  getOptGroup: false,
+  errorOpGroup: false,
+};
+
+const Opt = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_OPT_USER:
+      return {
+        ...state,
+        getOptUser: action.payload.data,
+        errorOptUser: action.payload.errorMessage,
+      };
+
+    case GET_OPT_GROUP:
+      return {
+        ...state,
+        getOptGroup: action.payload.data,
+        errorOptGroup: action.payload.errorMessage,
+      };
+    default:
+      return state;
+  }
+};
+
+export default Opt;
