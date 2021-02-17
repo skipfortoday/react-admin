3e83ec94dff62dd8479678bf8b2ddba4d749bf52
Rizqi Pratama <rev4rmy@gmail.com>
2021-02-17 14:35:09 +0700

    Update Validation USer

--- src/actions/optAction.js
+++ src/actions/optAction.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const GET_OPT_USER = "GET_OPT_USER";
+export const GET_OPT_GROUP = "GET_OPT_GROUP";
+
 
 export const getOptUser = () => {
   return (dispatch) => {
@@ -18,6 +20,31 @@ export const getOptUser = () => {
       .catch(function (error) {
         dispatch({
           type: GET_OPT_USER,
+          payload: {
+            data: false,
+            errorMessage: error.message,
+          },
+        });
+      });
+  };
+};
+
+export const getOptGroup = () => {
+  return (dispatch) => {
+    axios
+      .get("http://192.168.0.25:3001/api/optgroup")
+      .then(function (response) {
+        dispatch({
+          type: GET_OPT_GROUP,
+          payload: {
+            data: response.data,
+            errorMessage: false,
+          },
+        });
+      })
+      .catch(function (error) {
+        dispatch({
+          type: GET_OPT_GROUP,
           payload: {
             data: false,
             errorMessage: error.message,
--- src/validations/UserValidation.js
+++ src/validations/UserValidation.js
@@ -18,10 +18,6 @@ const UserValidation = (values) => {
   }
   if (!values.KodeCabang || values.KodeCabang=== "") {
     errors.KodeCabang = "Kode harus diisi";
-  } else if (values.KodeCabang.length >3 || values.KodeCabang.length <3 ) {
-    errors.KodeCabang = 'Masukan Kode Cabang Yang Benar';
-  } else if (values.KodeCabang !== 'SB1' && values.KodeCabang !== 'SB2' ) {
-    errors.KodeCabang = 'Masukan Kode Cabang Yang Benar Contoh SB1';
   } 
 
   if (!values.TglMasuk || values.TglMasuk=== "") {
@@ -31,21 +27,15 @@ const UserValidation = (values) => {
 
   if (!values.UserID || values.UserID === "") {
     errors.UserID = "UserID harus diisi"; 
-  }  else if (['SB1', 'SB2', 'SB3', 'SB4'].includes(values.UserID)) {
-    errors.UserID = 'User ID Sudah Digunakan';
-  }
+  } 
 
 
   if (!values.Pass || values.Pass === "") {
     errors.Pass = "Password harus diisi";
-  } else if (values.Pass.length > 6 || values.Pass.length < 6 ) {
-    errors.Pass = 'Harus 6 dijit bos';
-  }
+  } 
 
   if (!values.GroupID || values.GroupID === "") {
     errors.GroupID = "Group ID harus diisi";
-  } else if (values.GroupID.length >3 || values.GroupID.length <3 ) {
-    errors.GroupID = 'Masukan Group ID Yang Benar';
   }
 
 
