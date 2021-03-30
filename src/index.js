72ff1768ac1fd13d971cc242e4530eb736f6114d
Rizqi Pratama <43211197+skipfortoday@users.noreply.github.com>
2021-03-30 14:54:37 +0700

    Remove Redux

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import reducer from "./reducers";
 import thunk from "redux-thunk";
 
 //const store = createStore(reducer, compose(applyMiddleware(thunk)));
-const store = createStore(reducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(reducer, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
